94725935ec7e0fd784377648b4912ad0a05b3827
Minasuki <jossiel2023@gmail.com>
2023-07-03 00:16:15 -0700

    pinta todas las img de la api

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
+const cards = document.getElementById('cards');
+const templateCard = document.getElementById('template__card').content;
+const fragment = document.createDocumentFragment();
 
-document.addEventListener('DOMContentLoaded', ()=>{
-fetchData();
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
 })
 
 const fetchData = async () => {
     try {
-         const res = await fetch('./js/products.json');
-         const data = await res.json();
-         console.log(data);
+        const res = await fetch('./js/products.json');
+        const data = await res.json();
+        //  console.log(data);
+        pintarCards(data);
     } catch (error) {
         console.log(error);
     }
 }
+
+const pintarCards = (data) => {
+    console.log(data);
+    data.forEach(producto => {
+        templateCard.querySelector('h5').textContent = producto.title;
+        templateCard.querySelector('p').textContent = producto.price;
+        templateCard.querySelector('img').setAttribute('src', producto.image);
+        //templateCard.querySelector('.btn-dark').dataset.id = producto.id;
+
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);
+    });
+    cards.appendChild(fragment);
+};

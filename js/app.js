1531bf2664189943971aa4f36e23dacdba5f530f
Minasuki <jossiel2023@gmail.com>
2023-07-02 23:10:35 -0700

    estructurando mejor los css

--- js/app.js
+++ js/app.js
@@ -1,35 +1,14 @@
-const cards = document.getElementById('cards');
-const templateCard = document.getElementById('template__card').content;
-const fragment = document.createDocumentFragment();
 
+document.addEventListener('DOMContentLoaded', ()=>{
 
-let carrito = {};
-
-document.addEventListener('DOMContentLoaded', () => {
-    fetchData();
-    
-});
+})
 
 const fetchData = async () => {
     try {
-        const res = await fetch('api.json');
-        const data = await res.json();
-        pintarCards(data);
-         console.log(data)
+         const res = await fetch('products.json');
+         const data = await res.json();
+         console.log(data);
     } catch (error) {
         console.log(error);
     }
 }
-
-const pintarCards = (data) => {
-    console.log(data);
-    data.forEach(producto => {
-        templateCard.querySelector('h5').textContent = producto.title;
-        templateCard.querySelector('p').textContent = producto.precio;
-       
-
-        const clone = templateCard.cloneNode(true);
-        fragment.appendChild(clone);
-    });
-    cards.appendChild(fragment);
-};
